a31275c7e1e43421b65cb410cc072cc0d0ec7e94
Victor Anacleto <contatoanacletovictor@gmail.com>
2022-10-06 21:11:02 -0300

    06-10-22

---
+++ array_1.js
@@ -0,0 +1,18 @@
+const mostrar = console.log;
+
+let menos = [1, 4, 6];
+let elemento = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let conjunto = [3, 2, 4, 5, 6, 3];
+let conjuntos = [2, 4, 9, 8, 7, 4];
+
+
+const num = [1, 2, 3, 4, 5];
+const vezesDois = n => n * 2
+
+
+// iterando sobre o array
+for(let ind in num) {
+        mostrar(ind)
+}
+
+
